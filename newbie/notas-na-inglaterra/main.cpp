36277a6ab9f135cf572a8a7ea390018af1173f7a
Gabriel Torres <gaabrieltt7@gmail.com>
2024-03-25 17:44:20 -0300

    Notas na Inglaterra

---
+++ newbie/notas-na-inglaterra/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  if (n > 85)
+    cout << "A" << endl;
+  else if (n > 60)
+    cout << "B" << endl;
+  else if (n > 35)
+    cout << "C" << endl;
+  else if (n > 1)
+    cout << "D" << endl;
+  else
+    cout << "E" << endl;
+
+  return 0;
+}
